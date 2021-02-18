7218236f32fa2ceaed2892d27053ff3c307a4ea3
tvdewiel <tvdewiel@hotmail.com>
2021-02-18 11:33:19 +0100

    x

--- BierWinkel5/Bier.cs
+++ BierWinkel5/Bier.cs
@@ -9,7 +9,7 @@ namespace DrankWinkel1
     public class Bier : Drank
     {
         public Bier(double prijsPerStuk, string naam, Bierspecificatie bierspecifikatie, Setgrootte minimumHoeveelheid)
-            : base(prijsPerStuk,naam,minimumHoeveelheid, bierspecifikatie)
+            : base(prijsPerStuk,naam,minimumHoeveelheid,  bierspecifikatie)
         {
         }
         public override string ToString()
